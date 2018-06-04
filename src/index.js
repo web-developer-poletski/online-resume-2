cd508101359a81f38fead6c8f32b83ba36d8732b
eugene <web.developer.poletski@gmail.com>
2018-06-04 18:30:13 +1200

    Provide firebase functionality to root component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { firebaseAppConfig, resumeConfig } from './config';
+import Firebase from './firebase';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const firebase = new Firebase({ firebaseAppConfig, resumeConfig });
+
+ReactDOM.render(<App firebase={firebase} />, document.getElementById('root'));
 registerServiceWorker();
