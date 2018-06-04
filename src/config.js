6338f4baaaf75b6cb0d8102e22243b5a3f634fbd
eugene <web.developer.poletski@gmail.com>
2018-06-04 18:25:29 +1200

    Add firebase config

---
+++ src/config.js
@@ -0,0 +1,25 @@
+const {
+  REACT_APP_FIREBASE_API_KEY,
+  REACT_APP_AUTH_DOMAIN,
+  REACT_APP_DATABASE_URL,
+  REACT_APP_PROJECT_ID,
+  REACT_APP_STORAGE_BUCKET,
+  REACT_APP_MESSAGING_SENDER_ID,
+} = process.env;
+
+export const firebaseAppConfig = {
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_AUTH_DOMAIN,
+  databaseURL: REACT_APP_DATABASE_URL,
+  projectId: REACT_APP_PROJECT_ID,
+  storageBucket: REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_MESSAGING_SENDER_ID
+}
+
+export const resumeConfig = {
+  collectionsNames: [
+    'personal_info',
+    'careers',
+    'educations',
+  ],
+}
