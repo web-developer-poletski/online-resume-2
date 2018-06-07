be38a1572d44b9d7ef35254ab18be3720080c1d6
eugene <web.developer.poletski@gmail.com>
2018-06-07 22:50:17 +1200

    Add ui component Section tests

---
+++ src/ui/Section/tests/index.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { render, shallow } from 'enzyme';
+import Section from '../index';
+
+describe('<Section />', () => {
+  it('renders without crasing', () => {
+    shallow(<Section />);
+  });
+
+  it('renders children', () => {
+    const wrapper = shallow(
+      <Section>
+        <div className="child-a" />
+        <div className="child-b" />
+      </Section>
+    );
+
+    expect(wrapper.contains(<div className="child-a" />)).toBe(true);
+    expect(wrapper.contains(<div className="child-b" />)).toBe(true);
+  });
+
+  it('renders the prop title', () => {
+    const wrapper = render(<Section title="Section A" />);
+
+    expect(wrapper.text()).toContain('Section A');
+  });
+});
