3539954f8fe1881dca8473d321c22113c54dbd97
eugene <web.developer.poletski@gmail.com>
2018-06-10 21:22:59 +1200

    Add layout to Personal component

--- src/components/Personal/index.js
+++ src/components/Personal/index.js
@@ -1,10 +1,45 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 import { Section } from '../../ui';
 
+const Column = styled.div`
+  flex: 1;
+`;
+
+Personal.propTypes = {
+  firstName: PropTypes.string.isRequired,
+  lastName: PropTypes.string.isRequired,
+  loaction: PropTypes.string.isRequired,
+  personalStatement: PropTypes.string.isRequired,
+  photoURL: PropTypes.string.isRequired,
+};
+
 export default function Personal(props) {
   return (
-    <Section title="Personals">
+    <Section>
+      <Column>
+        <Section.Subtitle>
+          About
+        </Section.Subtitle>
+        <p>{props.personalStatement}</p>
+      </Column>
+
+      <Column>
+        <img src={props.photoURL} alt="" />
+      </Column>
+
+      <Column>
+        <Section.Subtitle>
+          Details
+        </Section.Subtitle>
+        <p>Name</p>
+        <p>{props.firstName} {props.lastName}</p>
+
+        <p>Location</p>
+        <p>{props.location}</p>
+      </Column>
     </Section>
   );
 }
