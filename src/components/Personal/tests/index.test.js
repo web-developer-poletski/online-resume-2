e63fc18ea668bfd25118a5dccd07e76eb3f816ed
eugene <web.developer.poletski@gmail.com>
2018-06-10 19:39:38 +1200

    Add tests for component Personal

---
+++ src/components/Personal/tests/index.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { render, shallow } from 'enzyme';
+
+import Personal from '../index';
+
+describe('<Personal />', () => {
+  it('renders without crashing', () => {
+    shallow(<Personal />);
+  });
+
+  it('renders a person`s first name', () => {
+    const wrapper = render(<Personal firstName="John"/>);
+    expect(wrapper.text()).toContain('John');
+  });
+
+  it('renders a person`s last name', () => {
+    const wrapper = render(<Personal lastName="Doe"/>);
+    expect(wrapper.text()).toContain('Doe');
+  });
+
+  it('renders a person`s personal statement', () => {
+    const wrapper = render(<Personal personalStatement="Hello world!"/>);
+    expect(wrapper.text()).toContain('Hello world!');
+  });
+
+  it('renders a person`s location', () => {
+    const wrapper = render(<Personal location="Planet Earth"/>);
+    expect(wrapper.text()).toContain('Planet Earth');
+  });
+});
