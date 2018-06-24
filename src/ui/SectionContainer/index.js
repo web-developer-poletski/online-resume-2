91d0a273b52a219329efbcf4d4d807e6d17bd2d6
eugene <web.developer.poletski@gmail.com>
2018-06-24 19:45:45 +1200

    Turn SectionContainer into non-flex, add display name

--- src/ui/SectionContainer/index.js
+++ src/ui/SectionContainer/index.js
@@ -3,7 +3,8 @@ import styled from 'styled-components';
 const SectionContainer = styled.div`
   margin: 0 auto;
   max-width: 800px;
-  display: flex;
 `;
+
+SectionContainer.displayName = 'SectionContainer';
 
 export default SectionContainer;
