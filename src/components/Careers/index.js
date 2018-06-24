95467c933e5cbae469f3f522e471d5e193692225
eugene <web.developer.poletski@gmail.com>
2018-06-24 19:54:44 +1200

    Add to Careers component capability to render distinct careers

--- src/components/Careers/index.js
+++ src/components/Careers/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import { Section } from '../../ui';
+import Career, { propTypes as careerPropTypes } from '../Career';
+
+Careers.propTypes = {
+  careers: PropTypes.arrayOf(PropTypes.shape(careerPropTypes)).isRequired,
+}
+
+Careers.defaultProps = {
+  careers: [],
+}
 
 export default function Careers(props) {
+  const careers = props.careers.map((career) => (
+    <Career key={career.id} {...career} />
+  ));
+
   return (
     <Section title="Careers">
+      {careers}
     </Section>
   );
 }
--- src/components/Careers/tests/index.test.js
+++ src/components/Careers/tests/index.test.js
@@ -1,15 +1,43 @@
 import React from 'react';
-import { render, shallow } from 'enzyme';
+import {
+  mount,
+  render,
+  shallow
+} from 'enzyme';
 
 import Careers from '../index';
+import Career from '../../Career';
 
 describe('<Careers />', () => {
-  it('renders without crashing', () => {
-    shallow(<Careers />);
-  });
+      it('renders without crashing', () => {
+        shallow( < Careers / > );
+      });
 
-  it('renders title', () => {
-    const wrapper = render(<Careers />);
-    expect(wrapper.text()).toContain('Careers');
-  });
-});
+      it('renders title', () => {
+        const wrapper = render( < Careers / > );
+        expect(wrapper.text()).toContain('Careers');
+      });
+
+      it('renders careers depending on the given data', () => {
+          const FAKE_CAREERS = [{
+              id: '1',
+              position: 'junior webmaster',
+              employerName: 'green valley',
+              employmentType: 'internship',
+              description: 'Started doing awesome stuff',
+            },
+            {
+              id: '2',
+              position: 'web developer',
+              employerName: 'grey cliffs',
+              employmentType: 'part-time',
+              description: 'Continued doing awesome stuff',
+              employerURL: 'https://greycliffs.co',
+              employerURLText: 'Grey Cliffs corporate website',
+            }
+          ];
+
+          const wrapper = shallow(<Careers careers={FAKE_CAREERS} />);
+            expect(wrapper.find(Career)).toHaveLength(FAKE_CAREERS.length);
+          });
+      });
