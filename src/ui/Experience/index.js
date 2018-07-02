39254d323872566785c0a9eeabd8acd405ec6a25
eugene <web.developer.poletski@gmail.com>
2018-07-02 21:27:13 +1200

    Add common ui component Experience

---
+++ src/ui/Experience/index.js
@@ -0,0 +1,101 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const ExperienceWrapper = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: flex-end;
+`;
+
+ExperienceWrapper.displayName = 'ExperienceWrapper';
+
+
+const ExperienceBrief = styled.h4`
+  flex: 75%;
+  order: 2;
+  margin-bottom: 12px;
+`;
+
+ExperienceBrief.displayName = 'ExperienceBrief';
+
+
+const ExperienceTitle = styled.h4`
+  flex: 25%;
+  order: 1;
+  margin-bottom: 12px;
+`;
+
+ExperienceTitle.displayName = 'ExperienceTitle';
+
+
+const ExperienceBriefDetail = styled.p`
+  flex: 75%;
+  order: 3;
+  margin: 0 0 8px 25%;
+`;
+
+ExperienceBriefDetail.displayName = 'ExperienceBriefDetail';
+
+
+const ExperienceDescription = styled.p`
+  flex: 75%;
+  order: 4;
+  margin: 0 0 0 25%;
+`;
+
+ExperienceDescription.displayName = 'ExperienceDescription';
+
+const ExperienceURL = styled.a.attrs({
+  href: ({ employerURL }) => employerURL || '',
+})`
+  flex: 75%;
+  order: 5; 
+  margin: 8px 0 0 25%;
+  text-decoration: none;
+  color: inherit;
+`;
+
+ExperienceDescription.displayName = 'ExperienceURL';
+
+export const propTypes = {
+  id: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  brief: PropTypes.string.isRequired,
+  briefDetail: PropTypes.string,
+  startDate: PropTypes.instanceOf(Date),
+  endDate: PropTypes.instanceOf(Date),
+  description: PropTypes.string.isRequired,
+  experienceURL: PropTypes.string,
+  experienceURLText: PropTypes.string,
+};
+
+Experience.propTypes = propTypes;
+
+Experience.defaultProps = {
+  id: '',
+  title: '',
+  brief: '',
+  description: '',
+};
+
+export default function Experience({
+  title,
+  brief,
+  briefDetail,
+  description,
+  experienceURL,
+  experienceURLText,
+}) {
+  return (
+    <ExperienceWrapper>
+      <ExperienceTitle>{title}</ExperienceTitle>
+      <ExperienceBrief>{brief}</ExperienceBrief>
+      <ExperienceBriefDetail>{briefDetail}</ExperienceBriefDetail>
+      <ExperienceDescription>{description}</ExperienceDescription>
+      <ExperienceURL href={experienceURL}>
+        {experienceURLText}
+      </ExperienceURL>
+    </ExperienceWrapper>
+  );
+} 
---
+++ src/ui/Experience/tests/index.test.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { mount, render, shallow } from 'enzyme';
+
+import Experience from '../index';
+
+describe('<Experience />', () => {
+  it('renders without crashing', () => {
+    shallow(<Experience />);
+  });
+
+  it('renders title', () => {
+    const wrapper = render(<Experience title="Some title" />);
+    expect(wrapper.text()).toContain('Some title');
+  });
+
+  it('renders brief', () => {
+    const wrapper = render(<Experience brief="First experience" />);
+    expect(wrapper.text()).toContain('First experience');
+  });
+
+  it('renders brief detail', () => {
+    const wrapper = render(<Experience briefDetail="(liked it)" />);
+    expect(wrapper.text()).toContain('(liked it)');
+  });
+
+  it('renders description', () => {
+    const wrapper = render(<Experience description="lorem ipsum" />);
+    expect(wrapper.text()).toContain('lorem ipsum');
+  });
+
+  it('renders experience url as a link', () => {
+    const wrapper = mount(
+      <Experience experienceURL="http://my-experience.me" />);
+
+    expect(wrapper.containsMatchingElement(
+      <a href="http://my-experience.me"></a>)).toBe(true);
+  });
+
+  it('renders experience url text', () => {
+    const wrapper = render(<Experience experienceURLText="my-experience" />);
+    expect(wrapper.text()).toContain('my-experience');
+  });
+});
