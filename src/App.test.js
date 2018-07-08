deb4fdbaf7987c6a6937d38f5af2dc3a896ca68e
eugene <web.developer.poletski@gmail.com>
2018-07-08 16:07:25 +1200

    Change App test to do shallow rendering instead of mounting

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {
-  mount
+  shallow,
 } from 'enzyme';
 
 import App from './App';
@@ -19,42 +19,42 @@ import {
 describe('<App />', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render( < App / > , div);
+    ReactDOM.render(<App />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 
   it('contains component Header', () => {
-    const wrapper = mount( < App / > );
-    expect(wrapper.containsMatchingElement(Header)).toBe(true);
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Header)).toHaveLength(1);
   });
 
   it('contains component Abilities', () => {
-    const wrapper = mount( < App / > );
-    expect(wrapper.containsMatchingElement(Abilities)).toBe(true);
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Abilities)).toHaveLength(1);
   });
 
   it('contains component Careers', () => {
-    const wrapper = mount( < App / > );
-    expect(wrapper.containsMatchingElement(Careers)).toBe(true);
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Careers)).toHaveLength(1);
   });
 
   it('contains component Educations', () => {
-    const wrapper = mount( < App / > );
-    expect(wrapper.containsMatchingElement(Educations)).toBe(true);
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Educations)).toHaveLength(1);
   });
 
   it('contains component Personal', () => {
-    const wrapper = mount( < App / > );
-    expect(wrapper.containsMatchingElement(Personal)).toBe(true);
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Personal)).toHaveLength(1);
   });
 
   it('contains component Projects', () => {
-    const wrapper = mount( < App / > );
-    expect(wrapper.containsMatchingElement(Projects)).toBe(true);
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Projects)).toHaveLength(1);
   });
 
   it('contains component Footer', () => {
-    const wrapper = mount( < App / > );
-    expect(wrapper.containsMatchingElement(Footer)).toBe(true);
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Footer)).toHaveLength(1);
   });
 });
