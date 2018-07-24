20282a56bfed2703526ee70594a9594cbbce761c
eugene <eugene.poletski@gmail.com>
2018-07-25 08:52:13 +1200

    Add projects list rendering to Projects component

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import { Section } from '../../ui';
+import Project, { propTypes as projectPropTypes } from '../Project';
+
+export const propTypes = {
+  projects: PropTypes.arrayOf(
+    PropTypes.shape(projectPropTypes),
+  ).isRequired,
+};
+
+export const defaultProps = {
+  projects: [],
+};
+
+Projects.propTypes = propTypes;
+Projects.defaultProps = defaultProps;
 
 export default function Projects(props) {
-  return (<Section title="Projects" ></Section>);
+  const projects = props.projects.map((project) => {
+    return (<Project key={project.id} {...project} />);
+  });
+
+  return (<Section title="Projects" >{projects}</Section>);
 }
--- src/components/Projects/tests/index.test.js
+++ src/components/Projects/tests/index.test.js
@@ -1,10 +1,40 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { render, shallow } from 'enzyme';
 
 import Projects from '../index';
+import Project from '../../Project';
 
 describe('<Projects />', () => {
   it('renders without crashing', () => {
     shallow(<Projects />);
+  });
+
+  it('renders title', () => {
+    const wrapper = render(<Projects />);
+    expect(wrapper.text()).toContain('Projects');
+  });
+
+  it('renders projects', () => {
+    const projects = [
+      {
+        id: '1',
+        projectName: 'Mobile pet',
+        projectImageURL: 'http://mobile.pet/thumbnail.bmp',
+        projectURL: 'https://mobile.pet',
+        projectURLText: 'Mobile pet Homepage',
+        description: 'Mobile pet description'
+      },
+      {
+        id: '2',
+        projectName: 'Roboninja',
+        projectImageURL: 'http://mystorage/roboninja.png',
+        projectURL: 'https://roboninja.com',
+        projectURLText: 'Robo Ninja - About',
+        description: 'blah-blah-blah...'
+      },
+    ];
+
+    const wrapper = shallow(<Projects projects={projects} />);
+    expect(wrapper.find(Project)).toHaveLength(projects.length);
   });
 });
