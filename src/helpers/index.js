5b01b399e06bfd39acbeaa36d2e4d8dac262d0bd
eugene <eugene.poletski@gmail.com>
2018-07-26 09:52:43 +1200

    Add jsdoc to helper sortCollectionByIds

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,3 +1,12 @@
+/**
+ * Sorts a collection of items containing "id" property
+ * to mirror the order of ids in an array passed as the second argument
+ *
+ * @param {array} collection Items collection (objects with a property "id")
+ * @param {array} sortedIds Sorted items ids
+ * 
+ * @returns {array} New array with the sorted by ids collection
+ */
 export function sortCollectionByIds(collection, sortedIds) {
   return sortedIds.map((sortedId) => collection.find(({ id }) => id === sortedId));
 }
