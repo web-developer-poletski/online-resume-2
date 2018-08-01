b990914cf79488736d5f5536f2d5a81b35d7a654
eugene <eugene.poletski@gmail.com>
2018-08-02 09:25:52 +1200

    Add basic style to NavBar

--- src/ui/NavBar/index.js
+++ src/ui/NavBar/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 import NavItem from '../NavItem';
+
+
+NavBar.NavItemList = styled.ul`
+  padding: 0;
+  list-style: none;
+`;
+NavBar.NavItemList.displayName = 'NavBar.NavItemList';
+
 
 export const propTypes = {
   navItems: PropTypes.array.isRequired,
@@ -18,5 +27,5 @@ export default function NavBar(props) {
   const navItems = props.navItems.map((navItem) =>
     <NavItem key={navItem.link} {...navItem} />);
 
-  return (<ul>{navItems}</ul>);
+  return (<NavBar.NavItemList>{navItems}</NavBar.NavItemList>);
 }
